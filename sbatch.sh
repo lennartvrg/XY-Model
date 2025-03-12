77bb3ec926948677b86694a3435502dcba1f88e9
Lennart Voorgang <lennart@voorgang.eu>
2025-03-12 12:39:35 +0100

    Optimized srun call

--- sbatch.sh
+++ sbatch.sh
@@ -8,4 +8,4 @@
 #SBATCH --partition=batch
 
 export RAYON_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-srun --exclusive target/release/XY-Model --run_id 1 --one 16 32 48 64 --two 16 32 48 64 80 96 112 128
+srun --cpu-bind=sockets --overcommit --exclusive target/release/XY-Model --run_id 1 --one 16 32 48 64 --two 16 32 48 64 80 96 112 128
